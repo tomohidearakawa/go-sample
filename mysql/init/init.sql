7ef1392e76f628898d1a85a2cfbe8e5225bc59b1
tomohidearakawa <tomohide.arakawa@zozo.com>
2024-04-16 20:27:45 +0900

    delete unuse file

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -15,6 +15,3 @@ CREATE TABLE IF NOT EXISTS `users2` (
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-INSERT INTO `users` (username, email, password) VALUES ('user1', 'user1@example.com', 'password1');
-INSERT INTO `users` (username, email, password) VALUES ('user2', 'user2@example.com', 'password2');
